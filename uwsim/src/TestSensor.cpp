09c344c4dbd1f307f7717581e86a1bc5c98a3d13
Babis Papakonstantinou <ch.papakon@yahoo.com>
2017-01-19 18:37:11 +0200

    Modified the TestSensor_Factory::applyConfig() method and fixed indentation of TestSensor.cpp.

--- uwsim/src/TestSensor.cpp
+++ uwsim/src/TestSensor.cpp
@@ -2,7 +2,7 @@
 #include <uwsim/TestSensor.h>
 
 TestSensor::TestSensor(TestSensor_Config * cfg) :
-	SimulatedDevice(cfg)
+SimulatedDevice(cfg)
 {
 	this->frequency = cfg->frequency;
 }
@@ -27,10 +27,28 @@ SimulatedDeviceConfig::Ptr TestSensor_Factory::processConfig(const xmlpp::Node* 
 	return SimulatedDeviceConfig::Ptr(cfg);
 }
 
-bool TestSensor_Factory::applyConfig(SimulatedIAUV * auv, Vehicle &vehicleChars, SceneBuilder *sceneBuilder, size_t iteration){}
+bool TestSensor_Factory::applyConfig(SimulatedIAUV * auv, Vehicle &vehicleChars, SceneBuilder *sceneBuilder, size_t iteration)
+{
+	if (iteration > 0)
+		return true;
+	for (size_t i = 0; i < vehicleChars.simulated_devices.size(); ++i)
+	{
+		if (vehicleChars.simulated_devices[i]->getType() == this->getType())
+		{
+			TestSensor_Config * cfg = dynamic_cast<TestSensor_Config *>(vehicleChars.simulated_devices[i].get());
+			if (cfg)
+			{
+				auv->devices->all.push_back(TestSensor::Ptr(new TestSensor(cfg)));
+			}
+			else
+				OSG_FATAL << "TestSensor device '" << vehicleChars.simulated_devices[i]->name << "' inside robot '"
+			<< vehicleChars.name << "' has empty cfg, discarding..." << std::endl;
+		}
+	}
+	return true;
+}
 
 std::vector<boost::shared_ptr<ROSInterface> > TestSensor_Factory::getInterface(ROSInterfaceInfo & rosInterface, std::vector<boost::shared_ptr<SimulatedIAUV> > & iauvFile){}
-
 
 #if ROS_VERSION_MINIMUM(1, 9, 0)
 // new pluginlib API in Groovy and Hydro
